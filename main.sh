54dcaa1084e9eb71beaee81bd6b86ea9aa280070
fzjcdt <11510785@mail.sustech.edu.cn>
2025-07-28 13:18:07 +0800

    update README.md

--- main.sh
+++ main.sh
@@ -1,9 +1,4 @@
-python main.py --dataset cifar10 --seed 0
-python main.py --dataset cifar10 --seed 1
-python main.py --dataset cifar10 --seed 2
+python main.py --dataset cifar10 --seed 0``
 python main.py --dataset cifar100 --seed 0
-python main.py --dataset cifar100 --seed 1
-python main.py --dataset cifar100 --seed 2
 python main.py --dataset tiny-imagenet --seed 0
-python main.py --dataset tiny-imagenet --seed 1
-python main.py --dataset tiny-imagenet --seed 2
+python main.py --dataset cifar10-eps16 --seed 0
