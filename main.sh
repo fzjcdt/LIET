aa835d342fadcc7a8256a46e6ce16edce27ba015
fzjcdt <11510785@mail.sustech.edu.cn>
2025-07-28 13:18:20 +0800

    update README.md

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-python main.py --dataset cifar10 --seed 0``
+python main.py --dataset cifar10 --seed 0
 python main.py --dataset cifar100 --seed 0
 python main.py --dataset tiny-imagenet --seed 0
 python main.py --dataset cifar10-eps16 --seed 0
